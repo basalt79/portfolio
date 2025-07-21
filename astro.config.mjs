1702185c751514aea33bf0a0af035b5008c4a900
Georg.Aigner <Georg.Aigner@5gsystems.com>
2025-07-21 14:30:37 +0200

    publish with  github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/portfolio"
-
-
+  site: 'https://basalt79.github.io',
+  base: "/portfolio/",
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
